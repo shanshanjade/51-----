33fc35ff5873c9430ad26d4898d11a17cb6e9e83
shanshanjade <88292198@qq.com>
2022-04-16 14:34:58 +0800

    master

--- 4-2动态数码管显示/build/Release/builder.mk
+++ 4-2动态数码管显示/build/Release/builder.mk
@@ -1,5 +1,5 @@
-CFLAGS := --std-c99 -c -mmcs51 --opt-code-speed -I.\..\include -I.\..\..\..\..\..\Keil_v5\C51\INC -I.\..\..\..\..\..\Keil_v5\C51\ISD51 -I.\src
-CXXFLAGS := -c -mmcs51 --opt-code-speed -I.\..\include -I.\..\..\..\..\..\Keil_v5\C51\INC -I.\..\..\..\..\..\Keil_v5\C51\ISD51 -I.\src
-ASMFLAGS := -l -o -s -I.\..\include -I.\..\..\..\..\..\Keil_v5\C51\INC -I.\..\..\..\..\..\Keil_v5\C51\ISD51 -I.\src
-LDFLAGS := --out-fmt-ihx -mmcs51 -L .\..\include -L .\..\..\..\..\..\Keil_v5\C51\LIB
+CFLAGS := PRINT(.\build\Release\${c}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\include;.\..\..\..\..\..\Keil_v5\C51\INC;.\..\..\..\..\..\Keil_v5\C51\ISD51;.\..\..\..\..\..\Keil\C51\INC;.\src) DEFINE(__UVISION_VERSION='526')
+CXXFLAGS := PRINT(.\build\Release\${cpp}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\include;.\..\..\..\..\..\Keil_v5\C51\INC;.\..\..\..\..\..\Keil_v5\C51\ISD51;.\..\..\..\..\..\Keil\C51\INC;.\src) DEFINE(__UVISION_VERSION='526')
+ASMFLAGS := PRINT(.\build\Release\${asm}.lst) SET(SMALL) INCDIR(.\..\include;.\..\..\..\..\..\Keil_v5\C51\INC;.\..\..\..\..\..\Keil_v5\C51\ISD51;.\..\..\..\..\..\Keil\C51\INC;.\src)
+LDFLAGS :=  PRINT(.\build\Release\4-2动态数码管显示.map) REMOVEUNUSED
 LDLIBS := 
--- 8-1串口向电脑发送数据/build/Debug/builder.mk
+++ 8-1串口向电脑发送数据/build/Debug/builder.mk
@@ -1,5 +1,5 @@
-CFLAGS := PRINT(.\build\Debug\${c}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\..\..\..\..\Keil_v5\C51\INC;.\src;.\..\include) DEFINE(__UVISION_VERSION='526')
-CXXFLAGS := PRINT(.\build\Debug\${cpp}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\..\..\..\..\Keil_v5\C51\INC;.\src;.\..\include) DEFINE(__UVISION_VERSION='526')
-ASMFLAGS := PRINT(.\build\Debug\${asm}.lst) SET(SMALL) INCDIR(.\..\..\..\..\..\Keil_v5\C51\INC;.\src;.\..\include)
+CFLAGS := PRINT(.\build\Debug\${c}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\..\..\..\..\Keil\C51\INC;.\src;.\..\include) DEFINE(__UVISION_VERSION='526')
+CXXFLAGS := PRINT(.\build\Debug\${cpp}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\..\..\..\..\Keil\C51\INC;.\src;.\..\include) DEFINE(__UVISION_VERSION='526')
+ASMFLAGS := PRINT(.\build\Debug\${asm}.lst) SET(SMALL) INCDIR(.\..\..\..\..\..\Keil\C51\INC;.\src;.\..\include)
 LDFLAGS :=  PRINT(.\build\Debug\8-1串口向电脑发送数据.map) REMOVEUNUSED
 LDLIBS := 
