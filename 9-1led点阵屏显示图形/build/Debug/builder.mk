57af0950d02d168245aedaa6aa0ce85a27177745
shanshanjade <88292198@qq.com>
2022-04-18 15:59:24 +0800

    master

--- 9-1led点阵屏显示图形/build/Debug/builder.mk
+++ 9-1led点阵屏显示图形/build/Debug/builder.mk
@@ -1,5 +1,5 @@
-CFLAGS := PRINT(.\build\Debug\${c}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\..\..\..\..\Keil_v5\C51\INC;.\src;.\..\include) DEFINE(__UVISION_VERSION='526')
-CXXFLAGS := PRINT(.\build\Debug\${cpp}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\..\..\..\..\Keil_v5\C51\INC;.\src;.\..\include) DEFINE(__UVISION_VERSION='526')
-ASMFLAGS := PRINT(.\build\Debug\${asm}.lst) SET(SMALL) INCDIR(.\..\..\..\..\..\Keil_v5\C51\INC;.\src;.\..\include)
+CFLAGS := PRINT(.\build\Debug\${c}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\..\..\..\..\Keil\C51\INC;.\src;.\..\include) DEFINE(__UVISION_VERSION='526')
+CXXFLAGS := PRINT(.\build\Debug\${cpp}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\..\..\..\..\Keil\C51\INC;.\src;.\..\include) DEFINE(__UVISION_VERSION='526')
+ASMFLAGS := PRINT(.\build\Debug\${asm}.lst) SET(SMALL) INCDIR(.\..\..\..\..\..\Keil\C51\INC;.\src;.\..\include)
 LDFLAGS :=  PRINT(.\build\Debug\9-1led点阵屏显示图形.map) REMOVEUNUSED
 LDLIBS := 
--- 9-1led点阵屏显示图形/src/main.c
+++ 9-1led点阵屏显示图形/src/main.c
@@ -1,40 +1,14 @@
 #include "REG52.H"
 #include "delay.h"
-
-/* 74HC595引脚定义 */
-sbit SER = P3 ^ 4;  //数据口
-sbit SCK = P3 ^ 6;  //上升沿移位
-sbit RCK = P3 ^ 5;  //上升沿锁存
-
-void _74hc595_WriteByte(unsigned char byte) {
-    unsigned char i;
-    for (i = 0; i < 8; i++) {
-        SER = byte & (0x80 >> i);
-        SCK = 1;
-        SCK = 0;
-    }
-    RCK = 1;                                                                                                                                                                                                                                                                                                                                                       
-    RCK = 0;
-}
-void matrixLED_ShowColumn(unsigned char column, unsigned char mydata){
-    _74hc595_WriteByte(mydata);
-    P0 = ~ (0x80 >> column);
-    delay(1);
-    P0 = 0xFF;
-}
+#include "matrixLED.h"
+char animation[] = {0x00, 0xFF, 0x89, 0xFF, 0x89, 0x89, 0xFF, 0x00, 0x1E, 0x22, 0x42, 0xFF, 0x42, 0x42, 0x22, 0x1E};
 void main() {
-    P0 = 0x00;
-    SCK = 0;
-    RCK = 0;
-    matrixLED_ShowColumn(7, 0xFF);
+    MatrixLED_Init();
+    unsigned char i;
+
     while (1) {
-        matrixLED_ShowColumn(0, 0x3C);
-        matrixLED_ShowColumn(1, 0x42);
-        matrixLED_ShowColumn(2, 0xA9);
-        matrixLED_ShowColumn(3, 0x85);
-        matrixLED_ShowColumn(4, 0x85);
-        matrixLED_ShowColumn(5, 0xA9);
-        matrixLED_ShowColumn(6, 0x42);
-        matrixLED_ShowColumn(7, 0x3C);
+        for (i = 0; i < 8; i++) {
+            matrixLED_ShowColumn(i, animation[i]);
+        }
     }
 }
