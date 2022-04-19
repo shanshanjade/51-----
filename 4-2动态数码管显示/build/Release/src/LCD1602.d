e8490eebc71ebf3ebea81e60d468ae53aba5f898
shanshanjade <88292198@qq.com>
2022-04-19 18:45:28 +0800

    master

--- 4-2动态数码管显示/build/Release/src/LCD1602.d
+++
@@ -1,2 +0,0 @@
-build\Release\src\LCD1602.rel: src\LCD1602.c \
- ../../../../../Keil_v5/C51/INC/REG52.h src\/LCD1602.h
--- 6-1矩阵键盘/build/Debug/builder.mk
+++
@@ -1,5 +0,0 @@
-CFLAGS := PRINT(.\build\Debug\${c}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\include;.\..\..\..\..\..\Keil_v5\C51\INC;.\src) DEFINE(__UVISION_VERSION='526')
-CXXFLAGS := PRINT(.\build\Debug\${cpp}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\include;.\..\..\..\..\..\Keil_v5\C51\INC;.\src) DEFINE(__UVISION_VERSION='526')
-ASMFLAGS := PRINT(.\build\Debug\${asm}.lst) SET(SMALL) INCDIR(.\..\include;.\..\..\..\..\..\Keil_v5\C51\INC;.\src)
-LDFLAGS :=  PRINT(.\build\Debug\6-1矩阵键盘.map) REMOVEUNUSED
-LDLIBS := 
