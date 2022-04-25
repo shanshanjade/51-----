50edd19a3d1a0b6f459339acbabedac080e8ced9
shanshanjade <88292198@qq.com>
2022-04-25 16:59:30 +0800

    12-1 中添加了外部中断P32

--- 12-1 AT24C02数据存储/src/main.c
+++ 12-1 AT24C02数据存储/src/main.c
@@ -3,8 +3,11 @@
 #include "key.h"
 #include "at24c02.h"
 #include "delay.h"
-unsigned char readData;
+unsigned char readData, count;
+void initEx0();
+
 void main() {
+    initEx0();
     LCD_Init();
     LCD_ShowString(1, 1, "at24c02");
     // at24c02_WriteByte(1, 1);
@@ -13,4 +16,18 @@ void main() {
     LCD_ShowNum(2, 1, readData, 4);
     while (1) {
     }
+}
+
+
+void initEx0() {
+    EA  = 1;	 //开启总中断
+    EX0 = 1;	 //开启0号外部中断
+    IT0 = 1;	 //设置外部中断触发方式 //0 - 低电平触发 //1 - 负跳变触发
+}
+void ex0_intr() interrupt 0 { 
+    //每进入一次中断，则次数加一
+    count = count + 1;
+    if(count == 10)
+        count = 0;
+    LCD_ShowNum(2, 10, count, 2);
 }
